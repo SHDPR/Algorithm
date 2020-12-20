2abb281e9bb3c54d510063d2057dce0b3aaefab8
SHDPR <shadowpur97@gmail.com>
2020-12-20 13:17:22 +0900

    12.20 AM

--- 10250.cpp
+++ 10250.cpp
@@ -2,17 +2,20 @@
 using namespace std;
 
 int main(){
-  int testcase = 0;
+  cin.tie(NULL);
+  cout.tie(NULL);
+  ios::sync_with_stdio(false);
+  int testcase;
   int H, W, N;
   cin >> testcase;
 
   for(int idx = 0; idx < testcase; idx++){
     cin >> H >> W >> N;
     if(N % H != 0){
-      cout << 100 * (N % H) + (N / H) + 1 << endl;
+      cout << 100 * (N % H) + (N / H) + 1 << '\n';
     }
     else{
-      cout << H * 100 + (N / H)<< endl;
+      cout << H * 100 + (N / H)<< '\n';
     }
   }
 }
