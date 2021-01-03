1d11185461b7a22dde8975cb1c25309f08d5a135
SHDPR <shadowpur97@gmail.com>
2021-01-03 14:27:17 +0900

    01.03 AM

--- 11653.cpp
+++ 11653.cpp
@@ -1,31 +1,19 @@
 #include <iostream>
 #include <cmath>
-#include <vector>
 using namespace std;
 
 
 int main(){
   int N = 0;
   cin >> N;
+  int idx = 2;
 
-  vector<int> eratos(N + 1, 1);
-
-  eratos[1] = 0;
-
-  for(int idx = 2; idx <= sqrt(N); idx++){
-    if(eratos[idx] == 0)
-      continue;
-    for(int jdx = 2; idx * jdx <= N; jdx++)
-      eratos[idx * jdx] = 0;
-  }
-
-
-  for(int idx = 2; idx < N + 1; idx++){
-    if(eratos[idx] == 1){
-      while(N % idx == 0){
-        cout << idx <<'\n';
-        N = N / idx;
-      }
+  while(N != 1){
+    if(N % idx == 0){
+      N /= idx;
+      cout << idx << '\n';
     }
+    else
+      idx++;
   }
 }
