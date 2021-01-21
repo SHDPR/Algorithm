68674aa5127872103c557fd458b967cf9067a403
SHDPR <shadowpur97@gmail.com>
2021-01-21 21:21:16 +0900

    01.21 AM

---
+++ 1920.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int binary_search(int start, int end, int target, int input[N]){
+  int flag = (start + end) / 2;
+}
+
+
+
+int main(){
+  int N, M;
+
+  // Input of N values
+  cin >> N;
+  int input[N];
+  for(int idx = 0; idx < N; idx++)
+    cin >> input[idx];
+
+  // Sort of N values
+  sort(input, input + N);
+
+  // Binary search for M values
+  cin >> M;
+  for(int idx = 0; idx < M; idx++){
+
+
+  }
+
+
+
+
+
+}
