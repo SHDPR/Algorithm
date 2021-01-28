653793986a81db3f2699543c910c59ce58aef715
SHDPR <shadowpur97@gmail.com>
2021-01-28 16:30:21 +0900

    01.28 AM

--- 10830.cpp
+++ 10830.cpp
@@ -1,5 +1,41 @@
 #include <iostream>
 #include <vector>
+using namespace std;
+
+typedef long long ll;
+typedef vector<vector<ll>> matrix;
+
+// Operator overloading for matrix
+matrix operator * (matrix &A, matrix &B){
+  int size = A.size();
+  matrix res(size, vector<ll>(size));
+
+  for(int idx = 0; idx < size; idx++){
+    for(int jdx = 0; jdx < size; jdx++){
+      for(int kdx = 0; kdx < size; kdx++)
+        res[idx][jdx] += A[idx][kdx] * B[kdx][jdx];
+      res[idx][jdx] %= 1000;
+    }
+  }
+  return res;
+}
+
+matrix power(matrix &A, ll B){
+  int size = A.size();
+  matrix res(size, vector<ll>(size));
+  // Set result as Identity matrix
+  for(int idx = 0; idx < size; idx++){
+    res[idx][idx] = 1;
+  }
+
+  while(B != 0){
+    if(B % 2 == 1)
+      res = res * A;
+    A = A * A;
+    B /= 2;
+  }
+  return res;
+}
 
 
 int main(){
@@ -7,15 +43,22 @@ int main(){
   cin.tie(NULL);
   cout.tie(NULL);
 
-  int N, B;
+  ll N, B;
   cin >> N >> B;
-  int matrix[N][N];
+  matrix input(N, vector<ll>(N));
 
   for(int idx = 0; idx < N; idx++){
     for(int jdx = 0; jdx < N; jdx++)
-      cout << matrix[idx][jdx];
+      cin >> input[idx][jdx];
   }
 
+  matrix result(N, vector<ll>(N));
+  result = power(input, B);
 
+  for(int idx = 0; idx < N; idx++){
+    for(int jdx = 0; jdx < N; jdx++)
+      cout << result[idx][jdx] << " ";
+    cout << '\n';
+  }
 
 }
--- 11444.cpp
+++ 11444.cpp
@@ -1,6 +1,67 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
+typedef long long ll;
+typedef vector<vector<ll>> matrix;
+
+// Operator overloading for matrix
+matrix operator * (matrix &A, matrix &B){
+  int size = A.size();
+  matrix res(size, vector<ll>(size));
+
+  for(int idx = 0; idx < size; idx++){
+    for(int jdx = 0; jdx < size; jdx++){
+      for(int kdx = 0; kdx < size; kdx++)
+        res[idx][jdx] += A[idx][kdx] * B[kdx][jdx];
+      res[idx][jdx] %= 1000000007;
+    }
+  }
+  return res;
+}
+
+matrix power(matrix &A, ll B){
+  int size = A.size();
+  matrix res(size, vector<ll>(size));
+  // Set result as Identity matrix
+  for(int idx = 0; idx < size; idx++){
+    res[idx][idx] = 1;
+  }
+
+  while(B != 0){
+    if(B % 2 == 1)
+      res = res * A;
+    A = A * A;
+    B /= 2;
+  }
+  return res;
+}
+
+/*
+
+Use the fact below
+
+| F(N+1)  F(N)  |   =   | 1   1 | ^ N
+| F(N)    F(N-1)|       | 1   0 |
+
+*/
+
 int main(){
-  
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+
+  ll B;
+  cin >> B;
+  matrix input(2, vector<ll>(2));
+  matrix result(2, vector<ll>(2));
+  input[0][0] = 1;
+  input[0][1] = 1;
+  input[1][0] = 1;
+  input[1][1] = 0;
+
+  result = power(input, B);
+
+  cout << result[1][0];
+
 }
